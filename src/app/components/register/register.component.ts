822fd747dab141275132848138f42e0d47373ec0
Andresss <davaccar@ufpso.edu.co>
2025-03-03 19:15:46 -0500

    Solucion a problemas de rutas y vistas del login y el register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { CommonModule } from '@angular/common'; 
+import { RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [LoginComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
